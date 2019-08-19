5c51f443148425ac06b639ee995a859cfc919d7f
chipdelmal <chipdelmal@gmail.com>
2019-08-19 09:43:17 -0700

    Adding sources to scripts

---
+++ fonts.py
@@ -0,0 +1,19 @@
+###############################################################################
+# Fonts
+#   Dedicated script to show the font options available to matplotlib
+###############################################################################
+# Source:
+#   http://jonathansoma.com/lede/data-studio/matplotlib/list-all-fonts-available-in-matplotlib-plus-samples/
+#   https://stackoverflow.com/questions/8753835/how-to-get-a-list-of-all-the-fonts-currently-available-for-matplotlib
+###############################################################################
+
+import matplotlib.font_manager
+from IPython.core.display import HTML
+
+def make_html(fontname):
+    str = "<p>{font}: <span style='font-family:{font}; font-size: 24px;'>{font}</p>"
+    return str.format(font=fontname)
+
+matplotlib.font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
+code = "\n".join([make_html(font) for font in sorted(set([f.name for f in matplotlib.font_manager.fontManager.ttflist]))])
+HTML("<div style='column-count: 2;'>{}</div>".format(code))
--- spectrogram.py
+++ spectrogram.py
@@ -1,25 +1,34 @@
+
+# https://matplotlib.org/users/text_props.html
+# https://matplotlib.org/api/_as_gen/matplotlib.colors.LinearSegmentedColormap.html#matplotlib.colors.LinearSegmentedColormap.from_list
+# 
+
 import array
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
 from pydub.utils import get_array_type
 from matplotlib.colors import LinearSegmentedColormap
 
-color = [i / 255 for i in [155, 40, 125]]
+(colorT, colorB) = (
+    [i / 255 for i in [155, 40, 125]],
+    [i / 255 for i in [125, 40, 155]]
+)
 colors = [color, (1, 1, 1), color]
 cm = LinearSegmentedColormap.from_list("red", colors, N=500)
 #cm = plt.cm.get_cmap('bwr')#('seismic')
 font = {
-    'family': 'sans-serif',
+    'fontname': 'DIN Condensed',
     'color':  'black',
-    'weight': 'light',
-    'size': 100,
-    'alpha': .05
+    'weight': 'ultralight',
+    'size': 75,
+    'alpha': .075
 }
 
 (AUD_PATH, OUT_PATH) = ("./audio/", "./out/")
-sound = AudioSegment.from_file(file=AUD_PATH + 'impossiblegermany.mp3')
+sound = AudioSegment.from_file(file=AUD_PATH + 'goldenDays.m4a')
 left = sound.split_to_mono()[0]
 right = sound.split_to_mono()[1]
+peak_amplitude = sound.max
 
 bit_depth = left.sample_width * 8
 array_type = get_array_type(bit_depth)
@@ -36,12 +45,12 @@ plt.scatter(
 )
 ax.axis('off')
 plt.text(
-    .5, .5, 'Impossible Germany', fontdict=font,
+    .5, .5, 'Golden Days', fontdict=font,
     horizontalalignment='center', verticalalignment='center',
     transform=ax.transAxes
 )
 plt.savefig(
     OUT_PATH + 'out.png',
-    dpi=100, bbox_inches='tight'
+    dpi=500, bbox_inches='tight'
 )
 plt.close()
